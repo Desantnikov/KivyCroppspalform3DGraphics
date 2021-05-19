9215883c269e6e3154dba3599fa57bf51f08bf6d
Desiatnykov <anton.desiatnykov@orderry.com>
2021-05-19 07:21:11 +0300

    call attempts

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.app import ObjectProperty
-
-from plyer import gps
+from android.permissions import request_permissions, Permission
+from plyer import gps, call
 from kivy.app import App
 from kivy.uix.button import Button
 from kivy.clock import mainthread
@@ -24,45 +24,15 @@ class MyApp(App):
     sms = None
     call = None
 
-    screen_manager = ObjectProperty()
-
     def request_android_permissions(self):
-        """
-        Since API 23, Android requires permission to be requested at runtime.
-        This function requests permission and handles the response via a
-        callback.
-        The request will produce a popup if permissions have not already been
-        been granted, otherwise it will do nothing.
-        """
-        from android.permissions import request_permissions, Permission
-
-        def callback(permissions, results):
-            """
-            Defines the callback to be fired when runtime permission
-            has been granted or denied. This is not strictly required,
-            but added for the sake of completeness.
-            """
-            print("callback start")
-            if all([res for res in results]):
-                print("callback. All permissions granted.")
-            else:
-                print("callback. Some permissions refused.")
-
-            print("callback end")
-        request_permissions([Permission.ACCESS_COARSE_LOCATION,
-                             Permission.ACCESS_FINE_LOCATION], callback)
-        # # To request permissions without a callback, do:
-        # request_permissions([Permission.ACCESS_COARSE_LOCATION,
-        #                      Permission.ACCESS_FINE_LOCATION])
+        request_permissions([Permission.ACCESS_COARSE_LOCATION, Permission.ACCESS_FINE_LOCATION])
 
     def build(self):
         try:
-            gps.configure(on_location=self.on_location,
-                          on_status=self.on_status)
+            gps.configure(on_location=self.on_location)
+
         except NotImplementedError:
-            import traceback
-            traceback.print_exc()
-            self.gps_status = 'GPS is not implemented for your platform'
+            print('GPS is not implemented for your platform')
 
         if platform == "android":
             print("gps.py: Android detected. Requesting permissions")
@@ -85,15 +55,11 @@ class MyApp(App):
         self.gps_location = '\n'.join(['{}={}'.format(k, v) for k, v in kwargs.items()])
         print("on_location end")
 
-    @mainthread
-    def on_status(self, stype, status):
-        print("on_status start")
-        self.gps_status = 'type={}\n{}'.format(stype, status)
-        print("on_status end")
-
     def sos_signal_activate(self):
-        print('\r\n SOS ACTIVATED\nnumber: {self.number}; sms: {self.sms}; call: {self.call}\r\n')
-        self.root.children[0].add_widget(Button(text=f'LOCATION: {self.gps_location}'))
+        print(f'\r\n SOS ACTIVATED\nnumber: {self.number}; sms: {self.sms}; call: {self.call}\r\n')
+        # self.root.children[0].add_widget(Button(text=f'LOCATION: {self.gps_location}'))
+        # call.makecall(tel=self.number)
+        call.makecall(tel=int(self.number))
 
     def save_and_return_to_main_menu(self, number, sms, call):
         self.number = number
--- makefile
+++ makefile
@@ -10,3 +10,7 @@ run_adb_grep_python:
 	@echo "Connecting to device via USB"
 	@export PATH=$PATH:/home/admin1/.buildozer/android/platform/android-sdk/platform-tools/adb
 	@adb logcat | grep python
+
+run:
+	@make build_and_deploy
+	@make run_adb_grep_python
