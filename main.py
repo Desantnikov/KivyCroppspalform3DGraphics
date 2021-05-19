1ae0bba31c807607adcc0b8d0af57e77077a22f2
Desiatnykov <anton.desiatnykov@orderry.com>
2021-05-19 05:15:45 +0300

    started ui

--- main.py
+++ main.py
@@ -1,24 +1,36 @@
 from kivy.app import App
-from kivy.properties import ObjectProperty
-from kivy.uix.gridlayout import GridLayout
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
 
 
-class Container(GridLayout):
+# Declare both screens
+class MenuScreen(Screen):
+    def menu_func(self):
+        print('\r\n MENU FUNC \r\n')
 
-    text_input = ObjectProperty()
-    button = ObjectProperty()
-    label = ObjectProperty()
 
-    def change_test(self):
-        self.button.text = self.text_input.text
+class SettingsScreen(Screen):
+    pass
 
 
 class MyApp(App):
-    def build(self):
+    number = None
+    sms = None
+    call = None
 
-        return Container()
+    def sos_signal_activate(self):
+        print('\r\n SOS ACTIVATED\nnumber: {self.number}; sms: {self.sms}; call: {self.call}\r\n')
+
+
+    def save_and_return_to_main_menu(self, number, sms, call):
+        self.number = number
+        self.sms = sms == '+'
+        self.call = call == '+'
+
+        self.root.current = 'menu'
+
+        print(f'\r\n SAVED:\r\nnumber: {self.number}; sms: {self.sms}; call: {self.call}\r\n')
 
 
 if __name__ == '__main__':
     MyApp().run()
-
