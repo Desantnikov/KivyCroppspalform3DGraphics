131fafa70642c7c18853271b8540a70b4eb45866
Anton <desitanikovwork@gmail.com>
2021-10-31 23:09:50 +0200

    cool background

--- main.py
+++ main.py
@@ -19,11 +19,17 @@ def front_side_gen(bottom_left_corner: Pos, size: int):
 
 cubes_lines = []
 
+
+ROW_LENGTH = 10
+POSITION_MULTIPLIER = 4
+
+
+
 for line_number in range(1, 11):
     cubes_line = [
-        Cube(**front_side_gen(bottom_left_corner=Pos((idx * 7) + line_number * 7, line_number * 7), size=10))# - idx / 2))
+        Cube(**front_side_gen(bottom_left_corner=Pos((idx * POSITION_MULTIPLIER) + line_number * POSITION_MULTIPLIER, line_number * POSITION_MULTIPLIER), size=5))# - idx / 2))
         for idx
-        in range(11, 0, -2)
+        in range(20, 0, -2)
     ]
     cubes_lines.extend(cubes_line)
 
@@ -35,38 +41,62 @@ class MyWidget(Widget):
         super(MyWidget, self).__init__(**kwargs)
 
         self.cube_sides_color_values = [
-             # right
-              # front
             (0.6, 0.6, 0.6),  # top
-
-            (1, 1, 1),# (120, 120, 120),
+            (1, 1, 1),
             (0.3, 0.3, 0.3),
         ]
 
-        texture = make_texture()
-        from calculations.cube import SIDE
+
+
         with self.canvas:
             for cube in reversed(cubes_lines):
                 for idx, side in enumerate(cube.SIDES_DRAWING_ORDER):
-                    cube.sides[side].draw()#texture=texture if side == SIDE.FRONT else None)
+                    cube.sides[side].draw()
                     Color(rgb=self.cube_sides_color_values[idx])
 
 
 class RootWidgetBoxLayout(FloatLayout):
     def __init__(self, **kwargs):
-
-        # Window.height = 1200
-
         super(RootWidgetBoxLayout, self).__init__(**kwargs)
+
+        self.bind(
+            size=self._update_rect,
+            pos=self._update_rect
+        )
 
         my_widget = MyWidget()
         self.add_widget(my_widget)
 
 
+        with self.canvas.before:
+            Color(rgb=(255, 255, 255))
+
+            left_bottom_corner = Pos(0, 800)
+
+            texture = make_texture()
+            self.rect = Rectangle(pos=self.pos, size=self.size, texture=texture)
+
+
+            # self.back_wall = Rectangle()
+            # self.rect = Rectangle(pos=self.pos, size=self.size)
+
+    def _update_rect(self, instance, value):
+
+        self.rect.pos = instance.pos
+        # self.rect.pos = 400
+        # x, y = self.rect.pos
+        # self.rect.pos = (x, y+425)
+
+        self.rect.size = instance.size
+
+
 class MyApp(App):
     def build(self):
-        Window.size = (1000, 800)
+        # Window.clearcolor = (1, 1, 1, 1)
+        Window.size = (1400, 800)
         Window.top = 100
+        Window.left = 100
+        # Window.pos(100,100)
 
         self.root = root = RootWidgetBoxLayout()
 
--- shadow_texture.py
+++ shadow_texture.py
@@ -3,21 +3,23 @@ from PIL import Image, ImageFilter, ImageDraw
 
 RAD_MULT = 0.25
 
-def make_texture(width=500):
-    gradient = Image.new('RGBA', (width, width), color=(1,255,1,1))
+def make_texture(width=600):
+    gradient = Image.new('RGBA', (width, width+200), color=(1,255,1,1))
     draw = ImageDraw.Draw(gradient)
 
-    for x in reversed(range(width)):
-        for y in reversed(range(width)):
+    for x in range(width + 400):
+        for y in range(width):
             start = (x, y)
             end = (y, x)
             draw.line([start, end],  (1, 1, 1, int(x)), width=1)
 
+    from PIL.Image import BILINEAR
+    gradient = gradient.rotate(angle=35, resample=BILINEAR,expand=True)
     # for line in range(width):
     #     draw.line([(line, line), (width, width)], (1,1,1,line), width=1)
-    buf = gradient.tobytes()
+    buf = bytes(gradient.tobytes())
 
-    texture = Texture.create(size=(500, 500), bufferfmt='ubyte')
+    texture = Texture.create(size=(width, width+200), bufferfmt='ubyte', callback=print)
     texture.wrap = 'clamp_to_edge'
     texture.blit_buffer(buf, colorfmt='rgba', bufferfmt='ubyte')
     # gradient.show()
